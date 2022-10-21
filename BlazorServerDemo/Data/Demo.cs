0b1294f5f35866dfc53eaa0cd5bd2ac60be50cfd
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-21 17:55:31 +0200

    Demo Class
    Created Demo class

---
+++ BlazorServerDemo/Data/Demo.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorServerDemo.Data
+{
+    public class Demo
+    {
+        public DateTime StartupTime { get; init; }
+
+        public Demo()
+        {
+            StartupTime = DateTime.Now;
+        }
+    }
+}
