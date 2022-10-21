a645201faeb953c94672e62f8bbf1715205cb6bd
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-21 19:06:20 +0200

    second interface class and unit tests
    Created second class that implements IDemo and Unit tests to test Process Demo class where class injected in constructor is created in Unit test class via interface.

--- BlazorServerDemo/Data/ProcessDemo.cs
+++ BlazorServerDemo/Data/ProcessDemo.cs
@@ -11,22 +11,7 @@
 
         public int GetDaysInMonth()
         {
-            return _demo.StartupTime.Month switch
-            {
-                1 =>31,
-                2 =>28,
-                3 =>31,
-                4 =>30,
-                5 =>31,
-                6 =>30,
-                7 =>31,
-                8 =>31,
-                9 =>30,
-                10 =>31,
-                11 =>30,
-                12 =>31,
-                _ => throw new IndexOutOfRangeException(),
-            };
+            return DateTime.DaysInMonth(_demo.StartupTime.Year,_demo.StartupTime.Month);
         }
     }
 }
---
+++ BlazorServerDemo/Data/UtcDemo.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorServerDemo.Data
+{
+    public class UtcDemo : IDemo
+    {
+        public DateTime StartupTime { get; init; }
+
+        public UtcDemo()
+        {
+            StartupTime = DateTime.UtcNow;
+        }
+    }
+}
