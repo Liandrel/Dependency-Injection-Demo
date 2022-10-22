4ec4260260ba2121805501f763ceb60ff3ba4fbc
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-22 15:45:02 +0200

    Service Dispose
    Added Service Dispose sample for Demo class

--- BlazorServerDemo/Data/Classes/Demo.cs
+++ BlazorServerDemo/Data/Classes/Demo.cs
@@ -2,13 +2,21 @@
 
 namespace BlazorServerDemo.Data.Classes
 {
-    public class Demo : IDemo, ILocalDemo
+    public class Demo : IDemo, ILocalDemo, IDisposable
     {
+        private readonly ILogger<Demo> _log;
+
         public DateTime StartupTime { get; init; }
 
-        public Demo()
+        public Demo(ILogger<Demo> log)
         {
             StartupTime = DateTime.Now;
+            _log = log;
+        }
+
+        public void Dispose()
+        {
+            _log.LogError($"Demo ({StartupTime.ToString("hh:mm:ss ffffff")}) was disposed of");
         }
     }
 }
