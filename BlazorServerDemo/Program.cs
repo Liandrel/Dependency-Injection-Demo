29a9c706acee85a1621beb7ac179cd24c260c855
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-22 15:24:16 +0200

    Service Registration Methods
    Added Samples of diffrent registration methods

--- BlazorServerDemo/Program.cs
+++ BlazorServerDemo/Program.cs
@@ -13,6 +13,15 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDemoInfo();
 
+//*****Samples******
+//builder.Services.AddTransient<Demo>();
+//builder.Services.AddTransient<IDemo, Demo>();
+//builder.Services.AddSingleton(new DemoWithData(5));
+//builder.Services.AddSingleton(new Demo());
+//builder.Services.AddTransient<IDemo>(i => new Demo());
+//builder.Services.AddTransient(i => new DemoWithData(4));
+
+
 
 var app = builder.Build();
 
